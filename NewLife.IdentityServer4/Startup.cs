2789e43b727199df39576aa07ee06a45a7cc178f
xxred <xxred0324@qq.com>
2019-04-26 18:02:28 +0800

    添加easyadmin的测试包地址，更新模块

--- NewLife.IdentityServer4/Startup.cs
+++ NewLife.IdentityServer4/Startup.cs
@@ -81,6 +81,7 @@ namespace NewLife.IdentityServer4
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+
         {
             app.UseApiExceptionHandler();
 
