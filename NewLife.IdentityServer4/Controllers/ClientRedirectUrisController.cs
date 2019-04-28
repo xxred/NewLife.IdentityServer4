977d8707c293b9bf9fc5747ceef7c51f68f23f08
xxred <xxred0324@qq.com>
2019-04-28 08:31:48 +0800

    添加几个控制器

---
+++ NewLife.IdentityServer4/Controllers/ClientRedirectUrisController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Easy.Admin.Areas.Admin.Controllers;
+using IdentityServer4.Models;
+using IdentityServer4.XCode.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewLife.IdentityServer4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientRedirectUrisController : EntityController<ClientRedirectUris>
+    {
+    }
+}
--- NewLife.IdentityServer4/Controllers/RouteController.cs
+++ NewLife.IdentityServer4/Controllers/RouteController.cs
@@ -4,12 +4,49 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
+using Newtonsoft.Json.Linq;
+using Easy.Admin.Areas.Admin.Controllers;
 namespace NewLife.IdentityServer4.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class RouteController : ControllerBase
+    public class RoutesController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var ctrls = typeof(RoutesController).Assembly.GetTypes()
+            .Where(w => typeof(BaseController).IsAssignableFrom(w.BaseType))
+            .ToList();
+
+            var list = new List<object>();
+
+            foreach (var ctrl in ctrls)
+            {
+                var name = ctrl.Name.TrimEnd("Controller");
+                var route = new
+                {
+                    path = name,
+                    component = "./src/views/layout/Layout",
+                    children = new[]
+                    {
+                        new
+                        {
+                            path = "index",
+                            template = $"<table-base table-name=\"{name}\" />",
+                            name= name,
+                            meta=new
+                            {
+                                title=ctrl.GetDescription()?? name,
+                                icon="international"
+                            }
+                        }
+                    }
+                };
+                list.Add(route);
+            }
+
+            return new JsonResult(list);
+        }
     }
 }
