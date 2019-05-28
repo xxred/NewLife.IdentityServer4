1bfaecbb8712daddada612742e6f3f3f2a5ac32d
笑笑 <xxred0324@qq.com>
2019-05-29 01:10:24 +0800

    添加客户端声明和属性控制器

---
+++ NewLife.IdentityServer4/Controllers/ClientClaimsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Easy.Admin.Areas.Admin.Controllers;
+using IdentityServer4.XCode.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewLife.IdentityServer4.Controllers
+{
+    [DisplayName("客户端声明")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientClaimsController : EntityController<ClientClaims>
+    {
+    }
+}
---
+++ NewLife.IdentityServer4/Controllers/ClientPropertiesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Easy.Admin.Areas.Admin.Controllers;
+using IdentityServer4.XCode.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewLife.IdentityServer4.Controllers
+{
+    [DisplayName("客户端属性")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientPropertiesController : EntityController<ClientProperties>
+    {
+    }
+}
