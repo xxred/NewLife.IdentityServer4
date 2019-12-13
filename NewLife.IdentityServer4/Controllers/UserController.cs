00743067f4620b67bc9f096190e4ec07eb9e7048
xxred <xxred0324@qq.com>
2019-12-13 18:09:00 +0800

    添加用户控制器，完善spa开发中间件设置

---
+++ NewLife.IdentityServer4/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using Easy.Admin.Areas.Admin.Controllers;
+using Easy.Admin.Areas.Admin.Models;
+using Easy.Admin.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace NewLife.IdentityServer4.Controllers
+{
+    /*
+     * 用户逻辑这块大部分还是自己实现吧，比如下面的默认密码等，或者添加其他功能
+     */
+
+    /// <summary>
+    /// 用户
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [DisplayName("用户")]
+    public class UserController : EntityController<ApplicationUser>
+    {
+        public override ApiResult Post(ApplicationUser value)
+        {
+            // 新增账号默认密码123456
+            value.Password = "123456".MD5();
+            return base.Post(value);
+        }
+    }
+}
